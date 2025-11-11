6c5865f2ae25c13f963f8ae5cfdf40250a3b85f1
ArneWinter02 <Arne.wntr@web.de>
2025-11-11 11:52:16 +0100

    Ein Paar Player Inputs
    Sowohl WSAD und die Arrow Keys sind für Bewegung verwendbar

--- Szees/player.gd
+++ Szees/player.gd
@@ -4,5 +4,7 @@ var direction: Vector2 = Vector2(1,1)
 var speed: int = 5
 
 func _physics_process(_delta: float) -> void:
-	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direction = Input.get_vector("left", "right", "up", "down")
 	position += direction * speed
+	if Input.is_action_just_pressed("print"):
+		print("Hi")
