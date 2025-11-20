4e6c5ad4953a6acc549b343506c26fdd6c194071
ArneWinter02 <Arne.wntr@web.de>
2025-11-20 15:18:03 +0100

    temp_comment

--- Scenes/enemy_spawner.gd
+++ Scenes/enemy_spawner.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-func damage(attack: Attack):
-	health -= attack_damage
-	if health <=0:
-		queue_free()
+#func damage(attack: Attack):
+	#health -= attack_damage
+	#if health <=0:
+	#	queue_free()
