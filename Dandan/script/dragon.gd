f1fda43b75b77d2805f1a4a59c440e2c129bc91e
Dannie293 <dandan.hinrichsen@volkswagen.de>
2025-12-01 22:09:41 +0100

    insel change something
    i just finished the enemy.

--- Dandan/script/dragon.gd
+++ Dandan/script/dragon.gd
@@ -5,7 +5,7 @@ const SPEED = 100
 
 var player_state
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector("left","right", "up","down")
 	
 	if direction.x == 0 and direction.y == 0:
